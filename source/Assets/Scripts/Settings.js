cf01bb38fc856d92852e26a605d8debb6a1196c0
Mediatedcub <cynara_ellis@yahoo.com>
2014-04-12 15:45:28 -0500

    Settings code is optional...

---
+++ source/Assets/Scripts/Settings.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+private var masterVolume : float = 1.0;
+private var sfxVolume : float = 1.0;
+ 
+function OnGUI()
+{
+    var groupWidth = 380;
+    var groupHeight = 110;
+     
+    var screenWidth = Screen.width;
+    var screenHeight = Screen.height;
+     
+    var groupX = ( screenWidth - groupWidth ) / 2;
+    var groupY = ( screenHeight - groupHeight ) / 2;
+     
+    GUI.BeginGroup( Rect( groupX, groupY, groupWidth, groupHeight ) );
+    GUI.Box( Rect( 0, 0, groupWidth, groupHeight ), "Audio Settings" );
+     
+    GUI.Label( Rect( 10, 30, 100, 30 ), "Master Volume" );
+    masterVolume = GUI.HorizontalSlider( Rect( 120, 35, 200, 30 ), masterVolume, 0.0, 5.0 );
+    GUI.Label( Rect( 330, 30, 50, 30 ), "(" + masterVolume.ToString("f2") + ")");
+ 
+    GUI.Label( Rect( 10, 70, 100, 30 ), "Effect Volume" );
+    sfxVolume = GUI.HorizontalSlider( Rect( 120, 75, 200, 30 ), sfxVolume, 0.0, 5.0 );
+    GUI.Label( Rect( 330, 70, 50, 30 ), "(" + sfxVolume.ToString("f2") + ")");
+ 	
+    GUI.EndGroup(); 
+    
+}
---
+++ source/Assets/Scripts/TitleGUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleGUI : MonoBehaviour 
+{
+	
+	void OnGUI ()
+	{
+	
+		AudioListener.volume = 1F;
+
+		
+		if (GUI.Button (new Rect (50, 10, 100, 100), "Settings")) 
+		{
+			print ("this is the volume settings, the curent level is  " + AudioListener.volume);
+		}
+
+		
+	}
+
+	
+
+}
