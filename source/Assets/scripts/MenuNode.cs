50d5aaad0f2f707d5b981ea6f4228fc9689daa39
Eric Wei <ericwei1991@gmail.com>
2014-04-12 16:23:55 -0500

    added basic hub layout, still need to link levels to each node

---
+++ source/Assets/scripts/MenuNode.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuNode : MonoBehaviour {
+	public GameObject prevNode; // if the user "moves left" cycle to this node
+	public GameObject nextNode; // if the user "moves right" cycle to this node
+	public string sceneName; // the scene to load
+	//public bool initialNode; // specify that this is the initial node
+
+	private bool _selected;
+
+	// Use this for initialization
+	void Start () {
+		//_selected = initialNode;		
+        _selected = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {        
+        /*
+		if(_selected) {
+			if(Input.GetButtonDown ("Horizontal")) {
+				// "Left"
+				if(Input.GetAxis("Horizontal") < 0 && prevNode != null) {
+					// switch selection to prevNode
+					prevNode.GetComponent<MenuNode>().SetSelected();
+					_selected = false;
+				}
+				// "Right"
+				else if(Input.GetAxis ("Horizontal") > 0 && nextNode != null) {
+					// switch selection to nextNode
+					nextNode.GetComponent<MenuNode>().SetSelected();
+					_selected = false;
+				}
+			}
+			else if(Input.GetKeyDown ("return") && sceneName != string.Empty) {
+				// change to the scene
+				Application.LoadLevel (sceneName);
+			}
+			else if(Input.GetKeyDown ("return")) {
+				Debug.Log ("return hit!");
+			}
+		}
+        */
+		SetColor ();
+
+        if (Input.GetMouseButtonDown(0) && _selected && sceneName != string.Empty)
+        {
+            Application.LoadLevel(sceneName);
+        }
+	}
+
+
+	void SetSelected() {
+		_selected = true;
+	}
+
+	void SetColor() {
+		if(_selected) {
+			this.renderer.material.color = Color.red;
+		}
+		else {
+			this.renderer.material.color = Color.blue;
+		}
+	}
+
+    void OnMouseOver()
+    {
+        this.renderer.material.color = Color.red;
+        _selected = true;
+    }
+
+    void OnMouseExit()
+    {
+        this.renderer.material.color = Color.blue;
+        _selected = false;
+    }
+}
---
+++ source/Assets/scripts/SceneSwitcher.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSwitcher : MonoBehaviour {
+	public string sceneName; // the scene to load
+	
+	private bool _selected;
+	
+	// Use this for initialization
+	void Start () {			
+		_selected = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {        
+		if (Input.GetMouseButtonDown(0) && _selected && sceneName != string.Empty)
+		{
+			Application.LoadLevel(sceneName);
+		}
+
+		if(Input.GetMouseButtonDown (0) && _selected) {
+			Debug.Log ("clicked!");
+		}
+	}
+	
+	void OnMouseOver()
+	{
+		Debug.Log ("mouse over");
+		_selected = true;
+	}
+	
+	void OnMouseExit()
+	{
+		Debug.Log ("mouse exit");
+		_selected = false;
+	}
+}
