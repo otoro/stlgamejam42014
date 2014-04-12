a15af6b66a9e8e1c208baf9b5f7f1a2690f7451e
sg <foo@bar.com>
2014-04-12 06:25:52 -0500

    Added basic movement to the player

---
+++ source/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+	private float speed;
+	
+	void Start () {
+		speed = 9.5f;
+	}
+	
+	void Update () {
+		float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		float y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+		
+		transform.Translate(x, y, 0);	
+	}
+}
