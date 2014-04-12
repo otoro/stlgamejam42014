95342f010b1a1819f4add9dbe7da85c8c8b3fdb5
sg <foo@bar.com>
2014-04-12 04:40:08 -0500

    Added a credits page

--- source/Assets/(c)scripts/CreditScroller.cs
+++ source/Assets/(c)scripts/CreditScroller.cs
@@ -1,15 +1,36 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class CreditScroller : MonoBehaviour {
+	public SpriteRenderer background;
+	public int fadeLength;
+	public GUIText credits;
+	public string titleSize;
 
-	// Use this for initialization
 	void Start () {
-	
+		credits.richText = true;
+		string creditsText = Resources.Load<TextAsset> ("Credits.txt").text;
+		creditsText = creditsText.Replace ("%TITLE_SIZE%", "" + titleSize);
+
+		credits.text = creditsText;
+
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		Camera mainCamera = Camera.main;
+		GameObject.Find("CreditsText").transform.Translate(Vector3.up * Time.deltaTime / 10);
+		mainCamera.transform.Translate(Vector3.down * Time.deltaTime);
+
+		if (mainCamera.transform.localPosition.y - mainCamera.orthographicSize - fadeLength < background.transform.localPosition.y - background.bounds.extents.y) {
+			Debug.Log ("Starting fade");
+			SendMessage("startFade");
+		}
+	}
+
+    void OnGUI() {
+        Event e = Event.current;
+        if (e.button == 0 && e.isMouse)
+			Application.LoadLevel ("Title");
 	}
 }
---
+++ source/Assets/(c)scripts/fadeOut.js
@@ -0,0 +1,49 @@
+﻿#pragma strict
+
+var fadeTexture : Texture2D;
+var fadeSpeed = 0.2;
+var drawDepth = -1000;
+
+private var isFadingIn = true;
+private var isFadingOut = false;
+ 
+private var alpha = 1.0;
+private var fadeDir = 1;
+ 
+function startFade() {
+    isFadingOut = true;
+}
+
+function doFade(alpha) {
+    GUI.color.a = alpha;
+ 
+    GUI.depth = drawDepth;
+ 
+    GUI.DrawTexture(Rect(0, 0, Screen.width, Screen.height), fadeTexture);
+}
+ 
+function OnGUI() {
+	if (isFadingIn) {
+        alpha -= fadeDir * fadeSpeed * Time.deltaTime;
+        alpha = Mathf.Clamp01(alpha);
+        
+        doFade(alpha);
+        
+		if (alpha <= 0.0) {
+			Debug.Log("Completed fading in");
+		    isFadingIn = false;
+		}
+	}
+
+    if (isFadingOut) {
+        alpha += fadeDir * fadeSpeed * Time.deltaTime;
+        alpha = Mathf.Clamp01(alpha);
+
+ 		doFade(alpha);
+        
+        if (alpha >= 1.0) {
+            Debug.Log("Reloading Scene");
+		    Application.LoadLevel("Credits");
+		}
+    }
+}
