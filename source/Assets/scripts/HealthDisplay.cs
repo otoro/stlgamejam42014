9ef5e79c7cf61907eb2bebe2b76c31d71f76f811
Eric Wei <ericwei1991@gmail.com>
2014-04-13 12:34:18 -0500

    added health display prefab

---
+++ source/Assets/scripts/HealthDisplay.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthDisplay : MonoBehaviour {
+	public GameObject healthSprite;
+	public Sprite health0;
+	public Sprite health1;
+	public Sprite health2;
+	public Sprite health3;
+	public Sprite health4;
+	public Sprite health5;
+	public Sprite health6;
+
+	private float health;
+
+	// Use this for initialization
+	void Start () {
+		health = 70.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		try {
+			SpriteRenderer sr = healthSprite.GetComponent<SpriteRenderer>();
+			if(health > 60.0f) {
+				sr.sprite = health0;
+
+			}
+			else if (health > 50.0f) {
+				sr.sprite = health1;
+			}
+			else if(health > 40.0f) {
+				sr.sprite = health2;
+			}
+			else if(health > 30.0f) {
+				sr.sprite = health3;
+			}
+			else if (health > 20.0f) {
+				sr.sprite = health4;
+			}
+			else if (health > 10.0f) {
+				sr.sprite = health5;
+			}
+			else if(health > 0.0f) {
+				sr.sprite = health6;
+			}
+		}
+		catch (UnityException e) {
+			Debug.LogException(e);
+		}
+	}
+
+	float getHealth() {
+		return health;
+	}
+
+	void decHealth(float amount) {
+		health -= amount;
+	}
+}
--- source/Assets/scripts/enemyAttackPatterns/directionalFire.cs
+++ source/Assets/scripts/enemyAttackPatterns/directionalFire.cs
@@ -14,15 +14,22 @@ public class directionalFire : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(_shotPauseTime == 0) {		
-			Sprite bullet = Sprite.Create (bulletTexture,
+		if(_shotPauseTime == 0) {
+			/*
+			Sprite bulletSprite = Sprite.Create (bulletTexture,
 			                               new Rect(this.transform.position.x, this.transform.position.y,
 			         								bulletTexture.width, bulletTexture.height),
 			                               new Vector2(bulletTexture.width / 2.0f, bulletTexture.height / 2.0f));
+			                               */
+			SpriteRenderer bulletRenderer = new SpriteRenderer();
+			bulletRenderer.sprite = Sprite.Create (bulletTexture,
+			                                       new Rect(this.transform.position.x, this.transform.position.y,
+			         bulletTexture.width, bulletTexture.height),
+			                                       new Vector2(bulletTexture.width / 2.0f, bulletTexture.height / 2.0f));
 		
 			Quaternion q = new Quaternion();
 			q.SetFromToRotation(this.transform.position, relativePoint.transform.position);
-			Instantiate(bullet, this.transform.position, q);
+			Instantiate(bulletRenderer, this.transform.position, q);
 			Debug.Log ("bullet created");
 			_shotPauseTime = fireRate;
 		}
