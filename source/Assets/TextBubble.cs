a5a3ff0365e804d4f5c08ae43efc85c47bc4b786
Eric Wei <ericwei1991@gmail.com>
2014-04-12 23:49:50 -0500

    improved mission blurb text display

--- source/Assets/TextBubble.cs
+++ source/Assets/TextBubble.cs
@@ -5,13 +5,24 @@ public class TextBubble : MonoBehaviour {
 	public float width;
 	public float height;
 	public string text;
+	public GameObject backgroundImage;
 	public GUIStyle style;
 
 	void OnGUI() {
-		float x = (Screen.width - width) / 2.0f;
-		float y = Screen.height * 0.9f - height;
-
-		GUI.TextArea (new Rect(x, y, width, height)
+		if(text != string.Empty) {
+			float x = (Screen.width - width) / 2.0f;
+			float y = Screen.height * 0.9f - height;
+			GUI.TextArea (new Rect(x, y, width, height)
 		              , text, style);
+
+			backgroundImage.renderer.enabled = true;
+		}
+		else {
+			backgroundImage.renderer.enabled = false;
+		}
+
+		if(backgroundImage.renderer.enabled) {
+			backgroundImage.transform.position.Set(Screen.width/2.0f, Screen.height * 0.9f, 0);
+		}
 	}
 }
